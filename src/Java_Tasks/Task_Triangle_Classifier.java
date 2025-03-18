bd47872a6eed47ad3be2be04329089e9e77b98f9
Supriya <supriyakotakonda12@gmail.com>
2025-03-18 20:19:04 +0530

    17th March 2025

--- src/Java_Tasks/Task_Triangle_Classifier.java
+++ src/Java_Tasks/Task_Triangle_Classifier.java
@@ -1,5 +1,7 @@
 package Java_Tasks;
 
+import java.util.Scanner;
+
 public class Task_Triangle_Classifier {
     public static void main(String[] args) {
 
@@ -11,10 +13,33 @@ public class Task_Triangle_Classifier {
           Use an if-else statement to classify the triangle.
          */
 
-
-
-
+        Scanner side1 = new Scanner(System.in);
+        System.out.println("Enter first side value");
+        float s1 = side1.nextFloat();
+        Scanner side2 = new Scanner(System.in);
+        System.out.println("Enter second side value");
+        float s2 = side2.nextFloat();
+        Scanner side3 = new Scanner(System.in);
+        System.out.println("Enter third side value");
+        float s3 = side3.nextFloat();
+        if(s1 == s2 && s2 == s3 ) {
+            System.out.println("Triangle is equilateral");
+        } else if (s1 == s2 || s2 == s3 || s1 ==s3){
+                System.out.println("isosceles triangle");
+            }
+            else {
+                System.out.println("scalene triangle");
+            }
+        }
+    // output
+        //    Enter first side value
+        //    4.56
+        //    Enter second side value
+        //    4.56
+        //    Enter third side value
+        //    6.78
+        //    isosceles triangle
 
 
     }
-}
+
